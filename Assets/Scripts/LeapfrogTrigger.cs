4c26ff2d5ff80e35c8f562e2d9570cca8b50653b
Mae Bridgeman <matthew@thebridgemans.net>
2024-02-20 17:45:11 -0700

    UI Update

--- Assets/Scripts/LeapfrogTrigger.cs
+++ Assets/Scripts/LeapfrogTrigger.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LeapfrogTrigger : MonoBehaviour
 {
+    public TextMeshProUGUI otherPlayerScore;
+    public TextMeshProUGUI winScreen;
     int enemyScore;
+
+    void Start(){
+        winScreen.text = "";
+    }    
 
     public void OnTriggerEnter(Collider trig){
         if (trig.transform.CompareTag("Player")){
@@ -14,6 +21,14 @@ public class LeapfrogTrigger : MonoBehaviour
             else scoreplayer = "Player 1";
 
             enemyScore++;
+
+            if (p1) otherPlayerScore.text = "Player Two Score: " + enemyScore;
+            else otherPlayerScore.text = "Player One Score: " + enemyScore;
+
+            if (enemyScore > 3) {
+                if (p1) winScreen.text = "Player two wins!";
+                else winScreen.text = "Player one wins!";
+            }
 
             Debug.Log(scoreplayer + " scored! Their score is " + enemyScore);
             
